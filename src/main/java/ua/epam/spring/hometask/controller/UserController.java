6343b9797ece75e0d10abf8d261bb63f9ffc4113
unlimit1984Mac <unlimit_rmc@mail.ru>
2017-11-14 17:30:43 +0100

    Add spring-context-support and Freemarker config

--- src/main/java/ua/epam/spring/hometask/controller/UserController.java
+++ src/main/java/ua/epam/spring/hometask/controller/UserController.java
@@ -5,36 +5,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import ua.epam.spring.hometask.domain.User;
 import ua.epam.spring.hometask.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/user")
+//@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @RequestMapping("/all")
-    public ModelAndView getAll(HttpServletRequest request){
+//    @Autowired
+//    public void setUserService(UserService userService) {
+//        this.userService = userService;
+//    }
 
-        ModelAndView mav = new ModelAndView();
+    @RequestMapping("/users")
+    public ModelAndView getAll(HttpServletRequest request) {
 
-
-        return mav;
-    }
-
-    @RequestMapping()
-    public ModelAndView getById(@RequestParam long id){
-
-        ModelAndView mav = new ModelAndView();
-
-        User user = userService.getById(id);
-        mav.addObject("user", user);
+        ModelAndView mav = new ModelAndView("users");
+        List<User> users = new ArrayList<>(userService.getAll());
+        mav.addObject("users", users);
 
         return mav;
     }
+
+//    @RequestMapping()
+//    public ModelAndView getById(@RequestParam long id) {
+//
+//        ModelAndView mav = new ModelAndView();
+//
+//        User user = userService.getById(id);
+//        mav.addObject("user", user);
+//
+//        return mav;
+//    }
 
 }
---
+++ src/main/webapp/WEB-INF/views/users.ftl
@@ -0,0 +1,30 @@
+<html>
+
+<head><title>User list</title></head>
+
+<body>
+
+<h3>User list</h3>
+
+<table style="border: solid 1px">
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Birthday</th>
+    </tr>
+<#list users as user>
+    <tr>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        <td>${user.birthday}</td>
+    </tr>
+
+</#list>
+
+</table>
+
+</body>
+
+</html>
