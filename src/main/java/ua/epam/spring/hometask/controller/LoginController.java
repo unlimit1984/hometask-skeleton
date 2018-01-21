2f1b78723007a3369c7b41a6b864627b0d2fd2b6
Vladimir_Vysokomorny <Vladimir_Vysokomornyi@epam.com>
2018-01-22 00:43:04 +0100

    [HW2] Fix csrf for uploading

--- src/main/java/ua/epam/spring/hometask/controller/LoginController.java
+++ src/main/java/ua/epam/spring/hometask/controller/LoginController.java
@@ -1,6 +1,7 @@
 package ua.epam.spring.hometask.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    //@RequestMapping(value = "/login", method = RequestMethod.GET)
+    @GetMapping("/login")
     public ModelAndView login(@RequestParam(value = "error", required = false) String error,
                               @RequestParam(value = "logout", required = false) String logout) {
         ModelAndView mav = new ModelAndView("login");
