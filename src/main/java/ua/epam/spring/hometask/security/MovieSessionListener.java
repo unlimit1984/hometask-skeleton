3ceeb353536377a96885eaafaa0d6a29a307045c
unlimit1984Hp <unlimit_rmc@mail.ru>
2018-01-25 23:35:15 +0100

    [HW3] Add csrf to Events and useful comments

--- src/main/java/ua/epam/spring/hometask/security/MovieSessionListener.java
+++ src/main/java/ua/epam/spring/hometask/security/MovieSessionListener.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpSessionListener;
 /**
  * Created by Vladimir_Vysokomorny on 22-Jan-18.
  */
+/*1st approach for changing session's timeout*/
 public class MovieSessionListener implements HttpSessionListener{
 
     @Override
     public void sessionCreated(HttpSessionEvent event) {
-        event.getSession().setMaxInactiveInterval(20);
+//        event.getSession().setMaxInactiveInterval(20);//20 seconds
+        event.getSession().setMaxInactiveInterval(20*60);//20 minutes
     }
 
     @Override
