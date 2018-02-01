deb1ac2400e75503841ffe0e443225e7e8735416
unlimit1984MacOs <unlimit_rmc@mail.ru>
2018-02-02 00:52:24 +0100

    [HW3] Add UserAccountService and AccountController

--- src/main/java/ua/epam/spring/hometask/repository/UserAccountRepository.java
+++ src/main/java/ua/epam/spring/hometask/repository/UserAccountRepository.java
@@ -5,8 +5,8 @@ import ua.epam.spring.hometask.domain.UserAccount;
 import java.util.Collection;
 
 public interface UserAccountRepository {
-    UserAccount save(UserAccount userAccount);
-    boolean delete(UserAccount userAccount);
-    UserAccount get(long id);
-    Collection<UserAccount> getAll();
+    UserAccount save(UserAccount userAccount, long userId);
+    boolean delete(UserAccount userAccount, long userId);
+    UserAccount get(long id, long userId);
+    Collection<UserAccount> getAll(long userId);
 }
